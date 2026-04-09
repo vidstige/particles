c3dca48261795a98e8686ce7210317b63cdd6352
Samuel Carlsson <samuel.carlsson@gmail.com>
2026-04-09 10:52:04 +0200

    refactor: renaming some variables/arguments

--- src/eq.rs
+++ src/eq.rs
@@ -1,33 +1,32 @@
 use crate::field::Field;
 
-pub fn solve_poisson_jacobi(right_hand_side: &Field<f32>, iterations: usize) -> Field<f32> {
-    let mut pressure = right_hand_side.new_like(0.0);
-    let mut next_pressure = right_hand_side.new_like(0.0);
-    let cell_size = right_hand_side.cell_size();
+pub fn solve_poisson_jacobi(rhs: &Field<f32>, iterations: usize) -> Field<f32> {
+    let mut x = rhs.new_like(0.0);
+    let mut next_x = rhs.new_like(0.0);
+    let cell_size = rhs.cell_size();
     let inverse_dx2 = 1.0 / cell_size.x.powi(2);
     let inverse_dy2 = 1.0 / cell_size.y.powi(2);
     let scale = 0.5 / (inverse_dx2 + inverse_dy2);
 
     for _ in 0..iterations {
-        for y in 0..right_hand_side.height() {
-            for x in 0..right_hand_side.width() {
-                let x = x as isize;
-                let y = y as isize;
-                let index = right_hand_side.index(x, y);
-                let left = pressure.get_wrapped(x - 1, y);
-                let right = pressure.get_wrapped(x + 1, y);
-                let down = pressure.get_wrapped(x, y - 1);
-                let up = pressure.get_wrapped(x, y + 1);
-                next_pressure.set_index(
+        for y in 0..rhs.height() {
+            for x_index in 0..rhs.width() {
+                let x_index = x_index as isize;
+                let y_index = y as isize;
+                let index = rhs.index(x_index, y_index);
+                let left = x.get_wrapped(x_index - 1, y_index);
+                let right = x.get_wrapped(x_index + 1, y_index);
+                let down = x.get_wrapped(x_index, y_index - 1);
+                let up = x.get_wrapped(x_index, y_index + 1);
+                next_x.set_index(
                     index,
-                    ((left + right) * inverse_dx2 + (down + up) * inverse_dy2
-                        - right_hand_side.get(index))
+                    ((left + right) * inverse_dx2 + (down + up) * inverse_dy2 - rhs.get(index))
                         * scale,
                 );
             }
         }
-        std::mem::swap(&mut pressure, &mut next_pressure);
+        std::mem::swap(&mut x, &mut next_x);
     }
 
-    pressure
+    x
 }
